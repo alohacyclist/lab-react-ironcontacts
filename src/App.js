86f1cdd7fff9a1a9795c0383c5f97be823882997
[Christian Mueller] <[c-mueller87@live.de]>
2022-03-18 16:21:35 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+import { useState } from 'react'
+import { FaAward, FaTrophy } from 'react-icons/fa'
 
 function App() {
+
+  const fiveContacts = contacts.slice(0, 5)
+  const [contact, setContact] = useState(fiveContacts)
+
+  let tableContent = () => {
+    return(contact.map((contact, index) => {
+      return (
+        <tr key={contact.id}>
+          <td><img style={{width: '200px'}} src={contact.pictureUrl} alt={contact.pictureUrl} /></td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity.toFixed()}</td>
+          <td>{(contact.wonEmmy) ? <FaAward /> : 'not yet'}</td>
+          <td>{(contact.wonOscar) ? <FaTrophy /> : 'not yet'}</td>
+          <button className='delete-btn' onClick={() => {deleteContact(contact.id)}}>DELETE</button>
+        </tr>)
+    	}))
+  }
+
+  const addContacts = () => {
+    let randomContact = contacts[Math.floor(Math.random() * (contacts.length))]
+    setContact([...contact, randomContact])
+  }
+
+  const sortContactsName = () => {
+    const currentContacts = [...contact]
+    currentContacts.sort((a, b) => {
+     if(a.name > b.name) return 1
+     if(a.name < b.name) return -1
+    })
+    return setContact(currentContacts)
+  }
+
+  const sortContactsPop = () => {
+    const currentContacts = [...contact]
+    currentContacts.sort((a, b) => {
+     if(a.popularity > b.popularity) {return 1;}
+     if(a.popularity < b.popularity) {return -1;}
+    })
+    setContact(currentContacts)
+  }
+
+  const deleteContact = contactId => {
+    const newContacts = contact.filter(contact => {return contact.id !== contactId})
+    setContact(newContacts)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <button className='options-btn' onClick={addContacts}>add random Contact</button>
+      <button className='options-btn' onClick={sortContactsPop}>sort by popularity</button>
+      <button className='options-btn' onClick={sortContactsName}>sort alphabetically</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Rating</th>
+          <th>won Emmy</th>
+          <th>won Oscar</th>
+        </tr>
+
+        <tbody>
+          {tableContent()}
+        </tbody>
+        
+    </table>
     </div>
   );
 }
